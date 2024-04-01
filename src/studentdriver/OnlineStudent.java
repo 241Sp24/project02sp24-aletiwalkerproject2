72199a78bdccdf5d6bcce593f5aea4d573d1f3e8
LWalk12 <S551736@nwmissouri.edu>
2024-03-31 19:18:59 -0500

    finished OnlineStudent
    finished all methods for Online Student and added comments

--- src/studentdriver/OnlineStudent.java
+++ src/studentdriver/OnlineStudent.java
@@ -10,4 +10,24 @@ package studentdriver;
  */
 public class OnlineStudent extends StudentFees {
 
+    //create instance variables
+    private int noOfMonths;
+    private double MONTHLY_FEE = 1245.25;
+
+    //method to assign values to instance variables uses method from StudentFees
+    public OnlineStudent(String studentName, int studentID, boolean isEnrolled, int noOfMonths) {
+        super(studentName, studentID, isEnrolled);
+        this.noOfMonths = noOfMonths;
+    }
+
+    //method to find cost of tuition
+    public double getPayableAmount() {
+        return noOfMonths * MONTHLY_FEE;
+    }
+
+    //toString method the builds off of StudentFees toString method
+    public String toString() {
+        return super.toString() + "\nNo of months: " + noOfMonths + "\nPayable amount: " + getPayableAmount();
+    }
+
 }
