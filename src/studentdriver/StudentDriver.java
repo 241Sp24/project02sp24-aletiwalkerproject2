0fecfd33e2f0f9731b166f5e3c9e17fe14570262
RathanAleti <aletirathanreddy77@gmail.com>
2024-04-05 11:01:50 -0500

    Done Ugstudent class

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package studentdriver;
-
+import java.util.*;
+import java.io.*;
 /**
  *
  * @author dianar
@@ -14,8 +15,26 @@ public class StudentDriver {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Project02");
+    public static void main(String[] args) throws FileNotFoundException {
+        // creating scanner 
+        
+      Scanner Myscanner= new Scanner(System.in);
+        System.out.println("Enter no of UG students : ");
+        System.out.println("Enter the no of Graduate students: ");
+        System.out.println("Enter the no of online students: ");
+        
+        // creating an array to store students 
+    StudentFees[]students = new StudentFees[12];
+     
+        File inputfile = new File ("input.csv");
+        Scanner input = new Scanner (inputfile);
+      while(input.hasNextLine()){
+          String[]strings = input.nextLine().split(",") ;
+          
+          int id =Integer.parseInt(strings[0]);
+          String name = strings[1];
+        
+    }
     }
 
 }
--- src/studentdriver/UGStudent.java
+++ src/studentdriver/UGStudent.java
@@ -9,5 +9,32 @@ package studentdriver;
  * @author s551736
  */
 public class UGStudent extends StudentFees {
-
+    
+   private double scholarshipAmount ;
+   private int coursesEnrolled ;
+   private boolean hasScholarship ;
+   private final double ADDITIONAL_FEE = 820.76;
+ public UGStudent (String studentName,int studentID,boolean isEnrolled,boolean hasScholarship ,double scholarshipAmount,int coursesEnrolled){
+     super (studentName , studentID, isEnrolled);
+     this.coursesEnrolled  = coursesEnrolled;  
+     this.hasScholarship = hasScholarship;
+     this .scholarshipAmount = scholarshipAmount;
+ }
+  public boolean hasScholarShip(){
+      return hasScholarship ;
+  }
+  public double getScholarShipAmount(){
+      return scholarshipAmount;
+   
+  }
+   public int getCoursesEnrolled(){
+       return coursesEnrolled;
+   }
+  public double getPayableAmount(){
+      return   ADDITIONAL_FEE + coursesEnrolled*3;
+  }
+  public String toString(){
+      return "courses Register : " + coursesEnrolled +"\n ";
+  }
+ 
 }
