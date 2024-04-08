4d204f961ddec9321e5fb251f2584dd0e525a329
RathanAleti <aletirathanreddy77@gmail.com>
2024-04-08 11:42:42 -0500

    mainclass

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -31,9 +31,42 @@ public class StudentDriver {
       while(input.hasNextLine()){
           String[]strings = input.nextLine().split(",") ;
           
+          // Typecasting the UGStudent class
           int id =Integer.parseInt(strings[0]);
           String name = strings[1];
+          boolean enrolled = Boolean.parseBoolean(strings[2]);
+          int courseenrolled = Integer.parseInt(strings[3]);
+          boolean HasScholarship = Boolean.parseBoolean(strings[4]);
+          double Amount = Double.parseDouble(strings[5]);
+         
+          // creating an object for UGstudent variable named UG
+          UGStudent UG = new UGStudent(name,id,enrolled,HasScholarship ,Amount,courseenrolled );
+          
+          //typecating for GSclass
+          int studentid=Integer.parseInt(strings[0]);
+          String studentname = strings[1];
+          boolean isstudentenrolled = Boolean.parseBoolean(strings[2]);
+          boolean isstudentGraduateAssistant = Boolean.parseBoolean(strings[3]);
+          String graduateType = strings[4] ;
+          int coursesenrolled = Integer.parseInt(strings[5]);
+          
+          // creating an object of GraduteStudent variable name  GS
+          GraduateStudent GS = new GraduateStudent(studentname,studentid,isstudentenrolled,isstudentGraduateAssistant,graduateType,coursesenrolled);
+          
+           // type casting for OnlineStudent class
+          int idofstudent=Integer.parseInt(strings[0]);
+          String studentsname = strings[1];
+          boolean isthestudentenrolled = Boolean.parseBoolean(strings[2]);
+            int numberofmonths = Integer.parseInt(strings[3]);
+            
+        // creating an object for online student named os
+        OnlineStudent os = new OnlineStudent (studentsname,idofstudent, isthestudentenrolled,numberofmonths );
         
+        // 
+        
+        
+          
+          
     }
     }
 
