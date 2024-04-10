6f1d9eddc866d89bcc9f4a7f0a2cecc1d1c47649
LWalk12 <S551736@nwmissouri.edu>
2024-04-09 21:57:46 -0500

    Fixed toString in UGStudent

--- src/studentdriver/UGStudent.java
+++ src/studentdriver/UGStudent.java
@@ -9,33 +9,39 @@ package studentdriver;
  * @author s551736
  */
 public class UGStudent extends StudentFees {
-    
-   private double scholarshipAmount ;
-   private int coursesEnrolled ;
-   private boolean hasScholarship ;
-   private final double ADDITIONAL_FEE = 820.76;
- public UGStudent (String studentName,int studentID,boolean isEnrolled,boolean hasScholarship ,double scholarshipAmount,int coursesEnrolled){
-     super (studentName , studentID, isEnrolled);
-     this.coursesEnrolled  = coursesEnrolled;  
-     this.hasScholarship = hasScholarship;
-     this .scholarshipAmount = scholarshipAmount;
- }
-  public boolean hasScholarShip(){
-      return hasScholarship ;
-  }
-  public double getScholarShipAmount(){
-      return scholarshipAmount;
-   
-  }
-   public int getCoursesEnrolled(){
-       return coursesEnrolled;
-   }
-  public double getPayableAmount(){
-      return   ADDITIONAL_FEE + coursesEnrolled*3;
-  }
-   @Override
-  public String toString(){
-      return "courses Register : " + coursesEnrolled +"\n ";
-  }
- 
+
+    private double scholarshipAmount;
+    private int coursesEnrolled;
+    private boolean hasScholarship;
+    private final double ADDITIONAL_FEE = 820.76;
+
+    public UGStudent(String studentName, int studentID, boolean isEnrolled, boolean hasScholarship, double scholarshipAmount, int coursesEnrolled) {
+        super(studentName, studentID, isEnrolled);
+        this.coursesEnrolled = coursesEnrolled;
+        this.hasScholarship = hasScholarship;
+        this.scholarshipAmount = scholarshipAmount;
+    }
+
+    public boolean hasScholarShip() {
+        return hasScholarship;
+    }
+
+    public double getScholarShipAmount() {
+        return scholarshipAmount;
+
+    }
+
+    public int getCoursesEnrolled() {
+        return coursesEnrolled;
+    }
+
+    public double getPayableAmount() {
+        return ADDITIONAL_FEE + coursesEnrolled * 3;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nScholarship: " + hasScholarShip() + "y\nScholarship amount: " + scholarshipAmount + "\ncourses Register : " + coursesEnrolled + "\nPayable amount: " + getPayableAmount();
+    }
+
 }
